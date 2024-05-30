2e12ecdb1779404e67bb4257870f88f14aa7aa29
Ashish <ashishpandey2062@gmail.com>
2024-05-30 20:48:20 +0545

    take time and practise

--- cinemaa/watchlist_app/api/views.py
+++ cinemaa/watchlist_app/api/views.py
@@ -2,6 +2,7 @@ from rest_framework.response import Response
 from watchlist_app.models import Movie
 from watchlist_app.api.serializers import MovieSerializer
 from rest_framework.decorators import api_view
+from rest_framework import status
 
 
 @api_view(['GET', 'POST'])
@@ -24,9 +25,30 @@ def movie_list(request):
 
 
 
-@api_view(['GET', 'POST'])
+@api_view(['GET', 'PUT','DELETE'])
 def movie_details(request,pk):
-    movie = Movie.objects.get(pk=pk)
-    serializer = MovieSerializer(movie)
-    return Response(serializer.data)
     
+    
+    if request.method == 'GET':
+        try:
+            movie = Movie.objects.get(pk=pk)
+            serializer = MovieSerializer(movie)
+            return Response(serializer.data)
+        except Movie.DoesNotExist:
+            return Response("Error movie not found",status=status.HTTP_404_NOT_FOUND) 
+    
+    
+    if request.method == 'PUT':
+        movie = Movie.objects.get(pk=pk)
+        serializer = MovieSerializer(movie,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else: 
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+            
+    if request.method == 'DELETE':
+        movie = Movie.objects.get(pk=pk)
+        movie.delete()  
+        return Response(status = status.HTTP_204_NO_CONTENT)
+        
