2fd26eb410bb74214c68b247bfdf58386419963a
Ashish <ashishpandey2062@gmail.com>
2024-06-01 19:54:19 +0545

    testinggg

--- cinemaa/watchlist_app/api/views.py
+++ cinemaa/watchlist_app/api/views.py
@@ -52,54 +52,54 @@ class MovieDetailAV(APIView):
             
 
 
-#   THIS IS THE ALTERNATIVE WAY TO WORK WIT
-#   API I.E. BY USING api_view DCORATORS..:..
+  THIS IS THE ALTERNATIVE WAY TO WORK WIT
+  API I.E. BY USING api_view DCORATORS..:..
 
 
-# @api_view(['GET', 'POST'])
+@api_view(['GET', 'POST'])
 
-# def movie_list(request):
+def movie_list(request):
     
-#     if request.method == 'GET':
-#         movies = Movie.objects.all()
-#         serializer = MovieSerializer(movies,many=True)
-#         return Response(serializer.data)
+    if request.method == 'GET':
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies,many=True)
+        return Response(serializer.data)
     
-#     if request.method == 'POST':
-#         serializer = MovieSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
+    if request.method == 'POST':
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
         
-#         else:
-#             return Response(serializer.errors)
+        else:
+            return Response(serializer.errors)
 
 
 
-# @api_view(['GET', 'PUT','DELETE'])
-# def movie_details(request,pk):
+@api_view(['GET', 'PUT','DELETE'])
+def movie_details(request,pk):
     
     
-#     if request.method == 'GET':
-#         try:
-#             movie = Movie.objects.get(pk=pk)
-#             serializer = MovieSerializer(movie)
-#             return Response(serializer.data)
-#         except Movie.DoesNotExist:
-#             return Response("Error movie not found",status=status.HTTP_404_NOT_FOUND) 
+    if request.method == 'GET':
+        try:
+            movie = Movie.objects.get(pk=pk)
+            serializer = MovieSerializer(movie)
+            return Response(serializer.data)
+        except Movie.DoesNotExist:
+            return Response("Error movie not found",status=status.HTTP_404_NOT_FOUND) 
     
     
-#     if request.method == 'PUT':
-#         movie = Movie.objects.get(pk=pk)
-#         serializer = MovieSerializer(movie,data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         else: 
-#             return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    if request.method == 'PUT':
+        movie = Movie.objects.get(pk=pk)
+        serializer = MovieSerializer(movie,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else: 
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
             
-#     if request.method == 'DELETE':
-#         movie = Movie.objects.get(pk=pk)
-#         movie.delete()  
-#         return Response(status = status.HTTP_204_NO_CONTENT)
+    if request.method == 'DELETE':
+        movie = Movie.objects.get(pk=pk)
+        movie.delete()  
+        return Response(status = status.HTTP_204_NO_CONTENT)
         
